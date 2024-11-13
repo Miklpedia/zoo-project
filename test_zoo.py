ea796b3a05db8b5c0b68cdc5a9eced23910170b7
Chayaphon Chaisangkha <Chayaphon.mick@gmail.com>
2024-11-13 16:06:27 +0000

    Added the test case

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,25 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+
     # Add your additional test cases here.
+
+    def test_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Error")
+
+    def test_ticket_price2(self):
+        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+
+    def test_ticket_price3(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+    
+    def test_ticket_price4(self):
+        self.assertEqual(self.zoo.get_ticket_price(25), 150)
+
+    def test_ticket_price5(self):
+        self.assertEqual(self.zoo.get_ticket_price(65), 100)
+   
+        
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: #allow age 0
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20:#allow age 20
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60:#count age 21
             return 150
-        elif age >= 60:
+        elif age > 60:#above 60 only
             return 100
+        elif age < 0:#error if age < 0
+            return ("Error")
